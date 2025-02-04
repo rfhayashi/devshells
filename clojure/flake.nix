cb7faa97701ac0a7ff89b20846c1d160e636380e
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-02-04 14:01:47 -0300

    add leiningen to clojure devshell

--- clojure/flake.nix
+++ clojure/flake.nix
@@ -10,7 +10,7 @@
     {
       devShells.${system} = {
         default = pkgs.mkShell {
-          packages = with pkgs; [ clojure clojure-lsp ];
+          packages = with pkgs; [ clojure clojure-lsp leiningen ];
         };
       };
     };
