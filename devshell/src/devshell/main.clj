474c2845ef1a946deb82cb1fbeaa2961a68c0dfa
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-02-05 17:54:40 -0300

    add help

--- devshell/src/devshell/main.clj
+++ devshell/src/devshell/main.clj
@@ -44,8 +44,20 @@
     (fs/write-lines envrc-path ["use flake"]))
   (shell "direnv allow"))
 
+(def init-command
+  {:fn init
+   :usage "devshell init <template>"
+   :description "Creates the files for the given <template>."
+   :opts {:args->opts [:template]
+          :require [:template]}})
+
 (defn update [_]
   (shell "nix flake update"))
+
+(def update-command
+  {:fn update
+   :usage "devshell update"
+   :description "Updates flake.lock"})
 
 (defn direnv-add [{{:keys [template]} :opts}]
   (when-file-does-not-exist [envrc-path ".envrc"]
@@ -54,6 +66,13 @@
     (fs/write-lines envrc-path [(format "use flake \"%s\"" (devshell-url template))] {:append true}))
   (shell "direnv allow"))
 
+(def direnv-add-command
+  {:fn direnv-add
+   :usage "devshell direnv add <template>"
+   :description "Adds the <template> to .envrc file"
+   :opts {:args->opts [:template]
+          :require [:template]}})
+
 (def devshell-rx (re-pattern (format "(%s)/[^?]*" flake-base-url)))
 
 (defn direnv-update [_]
@@ -61,23 +80,64 @@
     (fs/update-file (str envrc-path) str/replace devshell-rx (format "$1/%s" (devshell-revision))))
   (shell "direnv allow"))
 
+(def direnv-update-command
+  {:fn direnv-update
+   :usage "devshell direnv update"
+   :description "Update the .envrc file to point to templates latest versions"})
+
 (defn direnv-ignore [_]
   (with-path [git-path ".git"]
     (when (fs/exists? git-path)
       (fs/write-lines (fs/path git-path "info/exclude") [".direnv" ".envrc"] {:append true}))))
 
-(def direnv-commands
-  [{:cmds ["add"] :fn direnv-add :args->opts [:template]}
-   {:cmds ["update"] :fn direnv-update}
-   {:cmds ["ignore"] :fn direnv-ignore}])
+(def direnv-ignore-command
+  {:fn direnv-ignore
+   :usage "devshell direnv ignore"
+   :description "Adds direnv files to private git ignore"})
 
-(defn direnv [{:keys [args]}]
-  (cli/dispatch direnv-commands args))
+(defn print-help [{:keys [usage description opts]}]
+  (println (format "usage: %s" usage))
+  (println)
+  (println description)
+  (when (:spec opts)
+    (println (cli/format-opts opts))))
 
 (def commands
-  [{:cmds ["init"] :fn init :args->opts [:template]}
-   {:cmds ["direnv"] :fn direnv}
-   {:cmds ["update"] :fn update}])
+  [{:cmds ["init"] :command init-command}
+   {:cmds ["update"] :command update-command}
+   {:cmds ["direnv" "add"] :command direnv-add-command}
+   {:cmds ["direnv" "update"] :command direnv-update-command}
+   {:cmds ["direnv" "ignore"] :command direnv-ignore-command}])
 
+;; TODO list commands in help
+(defn help-table [commands]
+  (concat
+   [{:cmds [] :fn (fn [_] (print-help {:usage "devshell <command>" :description "Cli utility to manage devshells"}))}]
+   (map
+    (fn [{:keys [cmds command]}]
+      {:cmds cmds
+       :fn (fn [{{:keys [help]} :opts}]
+             (when help
+               (print-help command)))
+       :aliases {:h :help}})
+    commands)))
+
+(defn command-table [commands]
+  (map
+   (fn [{:keys [cmds] {:keys [fn opts]} :command}]
+     (assoc opts :cmds cmds :fn fn))
+   commands))
+
+(defn command-error-fn [{:keys [cause wrong-input msg]}]
+  (if msg
+    (println msg)
+    (case cause
+      :no-match (println (format "No such command: %s" wrong-input))))
+  (System/exit 1))
+
+;; TODO handle exceptions (only returning exit code) and add verbose mode
+;; when one wants to see the exception
 (defn -main [& args]
-  (cli/dispatch commands args))
+  (if (-> (cli/parse-args args {:aliases {:h :help}}) :opts :help)
+    (cli/dispatch (help-table commands) args {:error-fn command-error-fn})
+    (cli/dispatch (command-table commands) args {:error-fn command-error-fn})))
