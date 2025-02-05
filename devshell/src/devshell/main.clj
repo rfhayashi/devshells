3c019f5cab2acc637cbdb053246cca60f310b0c5
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-02-05 12:37:48 -0300

    more init stuff

--- devshell/src/devshell/main.clj
+++ devshell/src/devshell/main.clj
@@ -1,17 +1,29 @@
 (ns devshell.main
   (:require [babashka.cli :as cli]
-            [babashka.fs :as fs]))
+            [babashka.fs :as fs]
+            [babashka.process :refer [shell]]))
 
 (def ^:dynamic *working-dir* (fs/cwd))
 
-(defn init [_]
+(defn template-url [template]
+  (format "github:rfhayashi/devshells#%s" template))
+
+(defn init [{{:keys [template]} :opts}]
+  (shell "nix flake init -t" (template-url template))
   (let [envrc-path (fs/path *working-dir* ".envrc")]
     (when-not (fs/exists? envrc-path)
-      (fs/write-lines envrc-path ["use flake"]))))
+      (fs/write-lines envrc-path ["use flake"])))
+  (shell "direnv allow"))
+
+(def direnv-commands
+  [{:cmds ["add"]} ;; add a devshell template to .envrc
+   {:cmds ["update"]} ;; update revisions
+   {:cmds ["ignore"]} ;; adds direnv files to private git ignore
+   ])
 
 (def commands
-  [{:cmds ["init"] :fn init}   ;; devshell init
-   {:cmds ["add"]}    ;; devshell add clojure (creates .envrc file if does not exist and add to .gitignore)
+  [{:cmds ["init"] :fn init :args->opts [:template]}   ;; devshell init clojure
+   {:cmds ["direnv"]}  
    {:cmds ["update"]} ;; devshell update (if directory contains flake updates flake.lock)
    ])
 
