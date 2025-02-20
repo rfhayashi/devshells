921c4cc887af3865c27d7031ed3a9e3be24b160c
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-02-20 17:24:44 -0300

    add blank template

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   outputs = { ... } : {
     templates = {
+      blank = {
+        path = ./blank;
+        description = "Blank development environment";
+      };
       clojure = {
         path = ./clojure;
         description = "Clojure development environment";
