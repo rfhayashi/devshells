702584a769af93a31b372cbda8c4569e3256f623
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-03-13 17:35:50 -0300

    add ansible

---
+++ ansible/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Clojure development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ ansible ];
+        };
+      };
+    };
+}
