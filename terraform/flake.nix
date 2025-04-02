65a7b936beca5474be964a018a4987e547334d62
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-04-02 09:56:37 -0300

    terraform devshell

---
+++ terraform/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Terraform environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ terraform ];
+        };
+      };
+    };
+}
